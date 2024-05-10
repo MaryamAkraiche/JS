6974611374cb82fa35e615e6fff86fe3b7817de3
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-05-10 11:47:03 +0200

    added ex1 of asynchrone

---
+++ 3.Asynchrone/Exercise1/script.js
@@ -0,0 +1,19 @@
+const button = document.getElementById('jsonContent');
+button.addEventListener('click', function(){
+    fetch("data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const list = document.getElementById('list');
+            list.innerHTML = ' ';
+            data.forEach(item => {
+                const listItem = document.createElement('li');
+                listItem.textContent = item.name;
+                list.appendChild(listItem);
+            });
+        })
+	.catch((error) => {
+		console.log("There was an error!", error);
+	});
+});
+
+
